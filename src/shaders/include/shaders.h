168606f04ddf3350752762e9f42c6cc0bb596333
YasInvolved <jbaczyk27@gmail.com>
2026-02-04 02:15:35 +0100

    small corrections

--- src/shaders/include/shaders.h
+++ src/shaders/include/shaders.h
@@ -1,16 +1,20 @@
 #pragma once
 
-extern "C" char* vert_spv_start;
-extern "C" char* vert_spv_end;
+#include <stdint.h>
 
-extern "C" char* frag_spv_start;
-extern "C" char* frag_spv_end;
+extern "C" char vert_spv_start;
+extern "C" char vert_spv_end;
+
+extern "C" char frag_spv_start;
+extern "C" char frag_spv_end;
 
 namespace shaders
 {
-	const uint32_t* vert_code = reinterpret_cast<const uint32_t*>(&vert_spv_start);
-	const size_t vert_code_size = ((&vert_spv_end) - (&vert_spv_start)) / sizeof(uint32_t);
+	const uint32_t* vert_code_start = reinterpret_cast<const uint32_t*>(&vert_spv_start);
+	const uint32_t* vert_code_end = reinterpret_cast<const uint32_t*>(&vert_spv_end);
+	const size_t vert_code_size = (vert_code_end - vert_code_start) * sizeof(uint32_t);
 
-	const uint32_t* frag_code = reinterpret_cast<const uint32_t*>(&frag_spv_start);
-	const size_t frag_code_size = ((&frag_spv_end) - (&frag_spv_start)) / sizeof(uint32_t);
+	const uint32_t* frag_code_start = reinterpret_cast<const uint32_t*>(&frag_spv_start);
+	const uint32_t* frag_code_end = reinterpret_cast<const uint32_t*>(&frag_spv_end);
+	const size_t frag_code_size = (frag_spv_end - frag_spv_start) * sizeof(uint32_t);
 }
